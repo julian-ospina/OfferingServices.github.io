ce01f2e887637a84899cf2c0ba5e602b82a980d5
Julian Ospin <julian-ospina@hotmail.es>
2023-09-14 20:38:52 +0100

    new

--- dist/script.js
+++ dist/script.js
@@ -4,3 +4,39 @@ const menuToggle = document.getElementById('menu-toggle');
         menuToggle.addEventListener('click', () => {
             mobileMenu.classList.toggle('hidden');
         });
+
+        
+       
+
+     
+        const slides = document.querySelectorAll('.carousel-slide');
+        let currentSlide = 0;
+      
+        function showSlide(index) {
+          slides.forEach((slide, i) => {
+            if (i === index) {
+              slide.classList.add('active');
+            } else {
+              slide.classList.remove('active');
+            }
+          });
+        }
+      
+        function nextSlide() {
+          currentSlide++;
+          if (currentSlide >= slides.length) {
+            currentSlide = 0;
+          }
+          showSlide(currentSlide);
+        }
+      
+        function startCarousel() {
+          showSlide(currentSlide);
+          setInterval(() => {
+            nextSlide();
+          }, 5000); // Change slide every 5 seconds
+        }
+      
+        startCarousel(); // Start the carousel
+   
+      
