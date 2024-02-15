96b262d0732960f5e7b68bceee5158ccca217b19
julian-ospina <julian-ospina@hotmail.es>
2024-02-14 18:51:32 -0800

    fk script js

--- dist/es/script.js
+++ dist/es/script.js
@@ -45,10 +45,5 @@ leftBtn.addEventListener("click", moveLeft);
 
 // TW Elements is free under AGPL, with commercial license required for specific uses. See more details: https://tw-elements.com/license/ and contact us for queries at tailwind@mdbootstrap.com 
 // Initialization for ES Users
-import {
-    Carousel,
-    initTE,
-  } from "tw-elements";
-  
-  initTE({ Carousel });
+
 
