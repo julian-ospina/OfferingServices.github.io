b8d343b7a1e1a68b1084ce4de0521750cd83eceb
julian-ospina <julian-ospina@hotmail.es>
2024-02-14 19:12:00 -0800

    fixed

--- dist/es/script.js
+++ dist/es/script.js
@@ -42,8 +42,3 @@ leftBtn.addEventListener("click", moveLeft);
 
 
 
-
-// TW Elements is free under AGPL, with commercial license required for specific uses. See more details: https://tw-elements.com/license/ and contact us for queries at tailwind@mdbootstrap.com 
-// Initialization for ES Users
-
-
